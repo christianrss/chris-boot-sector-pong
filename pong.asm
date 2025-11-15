993529cd35cccc0b950a44c6a38afd8beff76866
Christian Rafael <christianparadix@gmail.com>
2025-11-15 00:40:09 -0300

    Draw CPU loop

--- pong.asm
+++ pong.asm
@@ -5,12 +5,15 @@ org 07C00h      ; Set bootsector to be at memory location hex 7C00h
 jmp setup_game  ; Jump over Variables section so we don't tryto execute it
 
 ;; CONSTANTS ----------
-VIDMEM equ 0B800h
+VIDMEM equ 0B800h   ; Color text mode VGA memory location
 ROWLEN equ 160      ; 80 Character row * 2 bytes each
+PLAYERX equ 4       ; Player X position
+CPUX    equ 156     ; CPU X Position
 
 ;; VARIABLES ----------
 drawColor: db 0F0h
-
+playerY:   dw 10    ; Start player Y position 10 rows down
+cpuY:      dw 10
 
 ;; LOGIC ===============
 setup_game:
@@ -31,17 +34,31 @@ game_loop:
     rep stosw
 
     ;; Draw mddle separating line
-    mov ah, [drawlColor]    ; White bg, black bg
+    mov ah, [drawColor]    ; White bg, black bg
     mov di, 78              ; Start at middle of 80 character row
-mov cl, 13                  ; 'Dashed' line - only draw every other row
+    mov cx, 13                  ; 'Dashed' line - only draw every other row
     .draw_middle_loop:
         stosw
         add di, 2*ROWLEN-2       ; Only draw every other row (80 Char * 2 bytes * 2 rows)
         loop .draw_middle_loop   ; Loops CX # of times
 
     ;; Draw player paddle
+    imul di, [playerY], ROWLEN   ; Y position is Y # rows * length of row
+;    add di, PLAYERX
+    mov cl, 5
+    .draw_player_loop:
+        mov [es:di+PLAYERX], ax
+;        stosw
+        add di, ROWLEN
+        loop .draw_player_loop
 
     ;; Draw CPU paddle
+    imul di, [cpuY], ROWLEN
+    mov cl, 5
+    .draw_cpu_loop:
+        mov [es:di+CPUX], ax
+        add di, ROWLEN
+        loop .draw_cpu_loop
 
     ;; Draw ball
 
