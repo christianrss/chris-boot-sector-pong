38bfe6da5f2435237dfb370a2b31e32867578bfc
Christian Rafael <christianparadix@gmail.com>
2025-11-29 00:16:38 -0300

    Move ball and cpu

--- pong.asm
+++ pong.asm
@@ -24,6 +24,8 @@ playerY:   dw 10    ; Start player Y position 10 rows down
 cpuY:      dw 10    ; Start cpu Y position 10 rows down
 ballX:     dw 66    ; Starting ball X position
 ballY:     dw 7     ; Starting ball Y position
+ballVelX:  db -1
+ballVelY:  db 1
 
 ;; LOGIC ===============
 setup_game:
@@ -71,7 +73,7 @@ game_loop:
     ;; Get Player input
     mov ah, 1           ; BIOS get keyboard status int 16h AH 01h
     int 16h
-    jz move_cpu         ; No key entered, don't check, move on
+    jz move_cpu_up         ; No key entered, don't check, move on
 
     cbw                 ; Zero out AH in 1 byte
     int 16h             ; BIOS get keystroke, scancode in AH, character in AL
@@ -85,37 +87,91 @@ game_loop:
     cmp ah, KEY_R
     je r_pressed
 
-    jmp move_cpu        ; Otherwise user entered some other key, move on
+    jmp move_cpu_up        ; Otherwise user entered some other key, move on
 
     ;; Move player paddle up
     w_pressed:
         dec word [playerY]  ; Move 1 row up
-        jge move_cpu        ; If player Y is at/above 0 (minimum Y value), then move on
+        jge move_cpu_up     ; If player Y is at/above 0 (minimum Y value), then move on
         inc word [playerY]  ; Else increment row # for collision check
-        jmp move_cpu
+        jmp move_cpu_up
 
     ;; Move player paddle down
     s_pressed:
         cmp word [playerY], SCREENH - PADDLEHEIGHT   ; Is player going to pass bottom of screen?
-        jg move_cpu                                  ; Yes, don'tmove
+        jg move_cpu_up                               ; Yes, don'tmove
         inc word [playerY]                           ; No, can move row down
-        jmp move_cpu
+        jmp move_cpu_up
 
     ;; Change Color of Middle line and paddles
     c_pressed:
         add byte [drawColor], 10h       ; Move to next VGA color
-        jmp move_cpu
+        jmp move_cpu_up
 
     ;; Reset game to initial state
     r_pressed:
         int 19h     ; Reloads the bootsector(in QEMU)
 
-
     ;; Move CPU
-    move_cpu:
+    move_cpu_up:
+        mov bx, [cpuY]
+        cmp bx, [ballY]     ; Is top of CPU paddle at or above the ball?
+        jle move_cpu_down   ; Yes, move on
+        dec word [cpuY]     ; No, move cpu up
+        jnz move_ball
+        inc word [cpuY]     ; If CPU hit top of area, move back down to correct
+    
+    move_cpu_down:
+        add bx, PADDLEHEIGHT
+        cmp bx, [ballY]         ; Is bottom of CPU paddle at or above the ball?
+        jge move_ball           ; Yes, move on
+        inc word [cpuY]         ; No, move cpu down
+        cmp word [cpuY], 24     ; Did cpu hit bottom of area?
+        jl move_ball            ; No, move on
+        dec word [cpuY]         ; Yes, move back up to correct
 
     ;; Move Ball
+    move_ball:
+        ;; Draw ball at current position
+        imul di, [ballY], ROWLEN
+        add di, [ballX]
+        mov word [es:di], 2000h
 
+        ;; Move ball to next position
+        mov bl, [ballVelX]      ; Ball X position change
+        add [ballX], bl
+        mov bl, [ballVelY]     ; Ball Y position change
+        add [ballY], bl
+
+    ;; Check collisions
+    check_hit_top:
+        cmp word [ballY], 0          ; Did ball hit top of screen? (Y minimum value = 0)
+        jg check_hit_bottom
+        neg byte [ballVelY]
+        jmp end_collision_checks
+
+    check_hit_bottom:
+        cmp word [ballY], 24
+        jl check_hit_player
+        neg byte [ballVelY]
+        JMP end_collision_checks
+
+    check_hit_player:
+        cmp word [ballX], PLAYERX        ; Is ball at same position as player paddle?
+        jne check_hit_cpu           ; No, move on
+        mov bx, [playerY]
+        cmp bx, [ballY]             ; Is top of player paddle equal or above the ball?
+        jg check_hit_cpu            ; No, move on
+        add bx, PADDLEHEIGHT        ; Check if hit bottom of player paddle
+        cmp bx, [ballY]
+        jl check_hit_cpu            ; Bottom of paddle is above ball, move on
+        neg byte [ballVelX]         ; Othewise hit ball, reverse X direction
+        jmp end_collision_checks
+
+
+    check_hit_cpu:
+
+    end_collision_checks:
     ;; Delay timer to next cycle
     mov bx, [046Ch]
     inc bx
